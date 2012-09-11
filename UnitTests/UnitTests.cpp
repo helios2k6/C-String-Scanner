f1f0ea6c8eac89a788b89e81b981e2e9bd5cff4b
Andrew Johnson <helios2k6@gmail.com>
2012-09-11 01:39:04 +0100

    updated unit tests

--- UnitTests/UnitTests.cpp
+++ UnitTests/UnitTests.cpp
@@ -2,6 +2,9 @@
 #include <iostream>
 #include <assert.h>
 #include "scanner.hpp"
+#include "boost/timer/timer.hpp"
+
+#define NUMER_OF_TESTS 1000
 
 using namespace std;
 using namespace scanner;
@@ -15,11 +18,17 @@ namespace{
 void unitTest1();
 void unitTest2();
 
+void timingTest();
+
 int main(){
 	cout << "Starting Unit Tests" << endl;
 	unitTest1();
 	unitTest2();
 	cout << "All Unit Tests Passed" << endl;
+	cout << "Running Timing Tests" <<endl;
+	timingTest();
+	cout << "End of Timing Tests" << endl;
+
 }
 
 void unitTest1(){
@@ -62,4 +71,31 @@ void unitTest2(){
 	free(r);
 	free(e);
 	free(w);
+}
+
+void timingTest(){
+	int * t = (int *)malloc(sizeof(int));
+	unsigned int * r = (unsigned int *)malloc(sizeof(unsigned int));
+	float * e = (float *)malloc(sizeof(float));
+	double * w = (double *)malloc(sizeof(double));
+
+	boost::timer::cpu_timer timer;
+
+	string temp("Some work that has to be done 1 2 3 4 5 6 -1 -2 -3 -4 -5 2.0 3.0 -4.0 -5.0");
+	Scanner scanner(temp);
+
+	timer.start();
+	for(long i = 0; i < NUMER_OF_TESTS; i++)
+	{
+		scanner.tryGetNextDouble(w);
+	}
+	timer.stop();
+
+
+	free(t);
+	free(r);
+	free(e);
+	free(w);
+
+	cout << "Time to run " << NUMER_OF_TESTS << " tests: " << timer.format();
 }
